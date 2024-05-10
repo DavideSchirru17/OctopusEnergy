b1059661dfa5570d5396db64e2c0c96da0358260
DavideSchirru17 <schirrudavide@icloud.com>
2024-05-10 18:25:24 +0100

    feat: creation feature2 - working on components header and footer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,17 @@ import "./Header.scss";
 function Header() {
   return (
     <>
-      <header className="header">HEADER</header>
+      <header className="header">
+        HEADER
+        <section className="header__container">
+          <img
+            src="../../assets/icons/icon-octopus-logo"
+            className="header__logo"
+            alt="logo octopus"
+          />
+          <div className="header__live-container"></div>
+        </section>
+      </header>
     </>
   );
 }
