fc3e98e06340ec1b71f209511d27435faef8458b
Bekarys Alimov <bekarysalimov@Bekaryss-MacBook-Pro.local>
2024-05-14 01:37:18 +0100

    leaderboard page first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import HomePage from "./PageRoute/HomePage/HomePage";
-
+import Leaderboard from "./PageRoute/LeaderBoard/LeaderBoard";
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
         </Routes>
         {/* <Header /> */}
         {/* <Footer /> */}
